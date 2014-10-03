9a63122126116f4fca72cdac38273a63b93bddbd
Borders, Casey <casey.borders@gsw-w.com>
2014-10-03 17:20:58 -0400

    Move Example behaviors into Standard Assets

---
+++ Assets/Standard Assets/BehaviorTree/Example Behaviors/ThreeTicksThenSucceed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public class ThreeTicksThenSucceed : LeafNode
+{
+	private int _ticks;
+
+	public override void Init()
+	{
+		_ticks = 0;
+	}
+
+	public override Result Process(Dictionary<System.String, System.Object> datastore)
+	{
+		_ticks++;
+		if(_ticks < 3) return Result.RUNNING;
+		else return Result.SUCCESS;
+	}
+}
