b8eeb62d2a93e10738238ebfaf76257197d61351
Borders, Casey <casey.borders@gsw-w.com>
2014-10-03 16:26:50 -0400

    Add a Monobehavior to act as the BehaviorTree root

---
+++ Assets/Standard Assets/BehaviorTree/BehaviorTree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorTree : MonoBehaviour
+{
+	public TextAsset _behaviorScript;
+
+	private Node _root;
+	private Dictionary<System.String, System.Object> _dataStore;
+
+	void Awake()
+	{
+		_dataStore = new Dictionary<System.String, System.Object>();
+		// Parse behavior script
+	}
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		_root.Process(_dataStore);
+	}
+}
