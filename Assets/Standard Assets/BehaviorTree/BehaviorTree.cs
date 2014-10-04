5cd33da0daa459ca1d62d258ce7f853e1774e602
Casey Borders <TheBeast.13@gmail.com>
2014-10-03 23:17:47 -0400

    Stop running after we get a result from the tree

--- Assets/Standard Assets/BehaviorTree/BehaviorTree.cs
+++ Assets/Standard Assets/BehaviorTree/BehaviorTree.cs
@@ -7,10 +7,13 @@ public class BehaviorTree : MonoBehaviour
 
 	private Node _root;
 	private Dictionary<System.String, System.Object> _dataStore;
+	private bool _done;
 
 	void Awake()
 	{
 		_dataStore = new Dictionary<System.String, System.Object>();
+		_done = false;
+
 		// Parse behavior script but for now hard code;
 		Selector selector = new Selector ();
 		selector.AddNode(new ThreeTicksThenFail());
@@ -24,6 +27,11 @@ public class BehaviorTree : MonoBehaviour
 
 	void Update()
 	{
-		_root.Process(_dataStore);
+		if(_done) return;
+		Node.Result result = _root.Process(_dataStore);
+		if(result != Node.Result.RUNNING)
+		{
+			_done = true;
+		}
 	}
 }
