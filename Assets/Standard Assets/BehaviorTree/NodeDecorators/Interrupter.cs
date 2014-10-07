6244e769c2961291be0887af7d15acfa8462ad1d
Borders, Casey <casey.borders@gsw-w.com>
2014-10-07 17:03:03 -0400

    Interrupter Node Decorator

---
+++ Assets/Standard Assets/BehaviorTree/NodeDecorators/Interrupter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Interrupter : NodeDecorator
+{
+	public override Result Process(Dictionary<String, Object> datastore)
+	{
+		// I'm not sure we need to implement this since we'll already call
+		// our child's Init in our own init.
+
+		return Result.SUCCESS;
+	}
+}
