c99ea8a4c1af9bc7425971aa42d59fcc7d0ba51b
Borders, Casey <casey.borders@gsw-w.com>
2014-10-07 17:21:52 -0400

    Starting Editor script to build Behavior Trees

---
+++ Assets/Standard Assets/BehaviorTree/BehaviorEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+
+public class BehaviorEditor : EditorWindow
+{
+	string myString = "Hello World";
+	bool groupEnabled;
+	bool myBool = true;
+	float myFloat = 1.23f;
+
+	[MenuItem ("Window/Behavior Tree Editor")]
+	public static void ShowWindow()
+	{
+		EditorWindow editor = EditorWindow.GetWindow(typeof(BehaviorEditor));
+		editor.title = "Behavior Tree";
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
+		GUILayout.BeginScrollView(Vector2.zero, GUIStyle.none);
+		GUILayout.BeginArea(new Rect(0, 0, 500, 500));
+
+
+		myString = EditorGUILayout.TextField ("Text Field", myString);
+		groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+		myBool = EditorGUILayout.Toggle ("Toggle", myBool);
+		myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+		EditorGUILayout.EndToggleGroup ();
+		GUILayout.EndArea();
+		GUILayout.EndScrollView();
+	}
+}
