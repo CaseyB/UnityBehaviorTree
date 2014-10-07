186c47bdb6decce548d58ff2719d37bd8f835765
Casey Borders <TheBeast.13@gmail.com>
2014-10-07 00:00:17 -0400

    Add Parallel CompositeNode

---
+++ Assets/Standard Assets/BehaviorTree/CompositeNodes/Parallel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Parallel.
+/// Process all child nodes until one returns FAILURE, then fail this node
+/// </summary>
+public class Parallel : CompositeNode
+{
+	private int _currentNode, _previousTick;
+	
+	public override void Init()
+	{
+		foreach(Node node in _nodes)
+		{
+			node.Init();
+		}
+	}
+	
+	public override Result Process(Dictionary<String, Object> datastore)
+	{
+		foreach(Node node in _nodes)
+		{
+			if(node.Process(datastore)) return Result.FAILURE;
+		}
+
+		return Result.RUNNING;
+	}
+}
