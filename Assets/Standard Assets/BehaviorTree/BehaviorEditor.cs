0070b1d320d33cb02a570f27db4960239a65b29c
Casey Borders <TheBeast.13@gmail.com>
2014-10-07 23:29:49 -0400

    Search in certain folders for Behaviors

--- Assets/Standard Assets/BehaviorTree/BehaviorEditor.cs
+++ Assets/Standard Assets/BehaviorTree/BehaviorEditor.cs
@@ -5,31 +5,35 @@ using System.Collections;
 
 public class BehaviorEditor : EditorWindow
 {
-	string myString = "Hello World";
-	bool groupEnabled;
-	bool myBool = true;
-	float myFloat = 1.23f;
+	private static Vector2 _scrollPosition;
+	private static String[] _builtIn;
 
 	[MenuItem ("Window/Behavior Tree Editor")]
 	public static void ShowWindow()
 	{
+		_scrollPosition = Vector2.zero;
+		_builtIn = AssetDatabase.FindAssets("t:monoscript", new string[] { "Assets/Standard Assets/BehaviorTree" });
+
 		EditorWindow editor = EditorWindow.GetWindow(typeof(BehaviorEditor));
 		editor.title = "Behavior Tree";
 	}
 
 	void OnGUI()
 	{
-		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
-		GUILayout.BeginScrollView(Vector2.zero, GUIStyle.none);
-		GUILayout.BeginArea(new Rect(0, 0, 500, 500));
+		_scrollPosition = GUILayout.BeginScrollView(_scrollPosition, GUIStyle.none);
 
+		foreach(String asset in _builtIn)
+		{
+			EditorGUILayout.LabelField(AssetDatabase.GUIDToAssetPath(asset));
+		}
 
-		myString = EditorGUILayout.TextField ("Text Field", myString);
-		groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-		myBool = EditorGUILayout.Toggle ("Toggle", myBool);
-		myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
-		EditorGUILayout.EndToggleGroup ();
-		GUILayout.EndArea();
+		String[] userCreated = AssetDatabase.FindAssets("t:monoscript", new string[] { "Assets/Scripts/Behaviors" });
+
+		foreach(String asset in userCreated)
+		{
+			EditorGUILayout.LabelField(AssetDatabase.GUIDToAssetPath(asset));
+		}
+
 		GUILayout.EndScrollView();
 	}
 }
